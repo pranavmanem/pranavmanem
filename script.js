655818467c368e91540299fa74a45227407bceb3
pranavmanem <pranavmanem@gatech.edu>
2017-05-16 15:26:43 -0400

    upload_1

---
+++ script.js
@@ -0,0 +1,30 @@
+
+/*$(document).ready(function () {
+    'use strict';
+    $('div').hover(function () {
+        
+        $('#mailImage').attr('src', 'images/mail_gray.png');
+        $('#facebookImage').attr('src', 'images/facebook_gray.png');
+        $('#linkedinImage').attr('src', 'images/linkedin_gray.png');
+        $(this).attr('background-color', 'blue');
+    });
+    
+});
+
+(function() {
+  var cards = document.querySelectorAll(".card.effect__click");
+  for ( var i  = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    clickListener( card );
+  }
+
+  function clickListener(card) {
+    card.addEventListener( "click", function() {
+      var c = this.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
+})();*/
+
+
+ 
